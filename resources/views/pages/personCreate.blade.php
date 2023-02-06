7a75b11bbb46e56911771b0e2e5bf9512ba03bab
Vale <oirelav95@gmail.com>
2023-02-06 18:00:18 +0100

    creo file personCreate

---
+++ resources/views/pages/personCreate.blade.php
@@ -0,0 +1,29 @@
+@extends('layouts.main-layout')
+
+
+@section('content')
+
+    <h1>
+        Create new person
+    </h1>
+    
+    <form method="POST" action="{{ route('person.store') }}">
+    @csrf
+
+    <label for="firstName">First Name: </label>
+    <input type="text" name="firstName">
+    <br>
+    <label for="lastName">Last Name: </label>
+    <input type="text" name="lastName">
+    <br>
+    <label for="dateOfBirth">Date of Birth: </label>
+    <input type="date" name="dateOfBirth">
+    <br>
+    <label for="height">Height: </label>
+    <input type="number" name="height" step="0.01">
+    <br>
+    <input type="submit" value="Create">
+</form>
+    
+    
+@endsection
