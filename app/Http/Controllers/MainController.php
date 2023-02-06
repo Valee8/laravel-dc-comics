cc0ecabf68c10a6417cb44e5972b56c5b37b0c18
Vale <oirelav95@gmail.com>
2023-02-06 18:15:24 +0100

    aggiungo metodi per la modifica di un elemento

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -52,4 +52,30 @@ class MainController extends Controller
 
         return redirect() -> route('home');
     }
+
+    public function personEdit(Person $person) {
+
+        return view('pages.personEdit', compact('person'));
+
+    }
+
+    public function personUpdate(Request $request, Person $person) {
+        $data = $request -> validate([
+            'firstName' => 'required|string|max:32',
+            'lastName' => 'required|string|max:32',
+            'dateOfBirth' => 'required|date',
+            'height' => 'decimal:1|min:1.4|max:2',
+        ]);
+
+        $person = new Person();
+        
+        $person -> firstName = $data['firstName'];
+        $person -> lastName = $data['lastName'];
+        $person -> dateOfBirth = $data['dateOfBirth'];
+        $person -> height = $data['height'];
+
+        $person -> save();
+
+        return redirect() -> route('home');
+    }
 }
