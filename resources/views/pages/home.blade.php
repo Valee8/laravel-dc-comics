398eea650ab0b010ac09f3993e5de4d7343eabe6
Vale <oirelav95@gmail.com>
2023-02-06 18:15:36 +0100

    aggiungo link a pagina per modifica elemento

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -15,7 +15,7 @@
         @foreach ($people as $person)
 
             <li>
-                <a href="{{ route('person.show', $person) }}">{{ $person -> firstName }} {{ $person -> lastName }}</a> - <a href="{{ route('person.delete', $person) }}">delete</a>
+                <a href="{{ route('person.show', $person) }}">{{ $person -> firstName }} {{ $person -> lastName }}</a> - <a href="{{ route('person.delete', $person) }}">delete</a> - <a href="{{ route('person.edit', $person) }}">Edit</a>
             </li>
         
         @endforeach
